6ce097bf3c32186593fbf9f0a7ceccb0cd75bab5
Shreyas <shrs.jha@gmail.com>
2024-02-25 08:52:14 -0500

    add rendering, complete q1 visualization

--- fit_data.py
+++ fit_data.py
@@ -6,33 +6,37 @@ import losses
 from pytorch3d.utils import ico_sphere
 from r2n2_custom import R2N2
 from pytorch3d.ops import sample_points_from_meshes
-from pytorch3d.structures import Meshes
+from pytorch3d.structures import Meshes, Pointclouds
 import dataset_location
 import torch
-
-
-
-
-
+from render import threeDObject, renderObjects
+import pytorch3d
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+import os
+from pathlib import Path
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Model Fit', add_help=False)
     parser.add_argument('--lr', default=4e-4, type=float)
-    parser.add_argument('--max_iter', default=100000, type=int)
+    parser.add_argument('--max_iter', default=20000, type=int)
     parser.add_argument('--type', default='vox', choices=['vox', 'point', 'mesh'], type=str)
     parser.add_argument('--n_points', default=5000, type=int)
     parser.add_argument('--w_chamfer', default=1.0, type=float)
     parser.add_argument('--w_smooth', default=0.1, type=float)
     parser.add_argument('--device', default='cuda', type=str) 
+    parser.add_argument('--output_path', default=Path(os.getcwd())/'data'/'shreyasj'/'1', type=Path)
     return parser
 
-def fit_mesh(mesh_src, mesh_tgt, args):
+def fit_mesh(mesh_src, mesh_tgt, args, tol=1e-4):
     start_iter = 0
     start_time = time.time()
-
     deform_vertices_src = torch.zeros(mesh_src.verts_packed().shape, requires_grad=True, device='cuda')
     optimizer = torch.optim.Adam([deform_vertices_src], lr = args.lr)
-    print("Starting training !")
+    global q
+    srcs = []
+    qidx = 0
     for step in range(start_iter, args.max_iter):
         iter_start_time = time.time()
 
@@ -54,18 +58,28 @@ def fit_mesh(mesh_src, mesh_tgt, args):
         iter_time = time.time() - iter_start_time
 
         loss_vis = loss.cpu().item()
+        if loss_vis < tol:
+            print('Converged!')
+            break
+        
+        if step%q[qidx]==0:
+            srcs.append((step,new_mesh_src.clone()))
+            qidx = min(qidx+1, len(q)-1)
 
         print("[%4d/%4d]; ttime: %.0f (%.2f); loss: %.3f" % (step, args.max_iter, total_time,  iter_time, loss_vis))        
     
     mesh_src.offset_verts_(deform_vertices_src)
 
     print('Done!')
+    return srcs
 
-
-def fit_pointcloud(pointclouds_src, pointclouds_tgt, args):
+def fit_pointcloud(pointclouds_src, pointclouds_tgt, args, tol=1e-4):
     start_iter = 0
     start_time = time.time()    
     optimizer = torch.optim.Adam([pointclouds_src], lr = args.lr)
+    global q
+    srcs = []
+    qidx = 0
     for step in range(start_iter, args.max_iter):
         iter_start_time = time.time()
 
@@ -79,20 +93,31 @@ def fit_pointcloud(pointclouds_src, pointclouds_tgt, args):
         iter_time = time.time() - iter_start_time
 
         loss_vis = loss.cpu().item()
+        if loss_vis < tol:
+            print('Converged!')
+            break
+        
+        if step%q[qidx]==0:
+            srcs.append((step,pointclouds_src.clone()))
+            qidx = min(qidx+1, len(q)-1)
 
         print("[%4d/%4d]; ttime: %.0f (%.2f); loss: %.3f" % (step, args.max_iter, total_time,  iter_time, loss_vis))
     
     print('Done!')
+    return srcs
 
 
-def fit_voxel(voxels_src, voxels_tgt, args):
+def fit_voxel(voxels_src, voxels_tgt, args, tol=1e-4):
     start_iter = 0
     start_time = time.time()    
     optimizer = torch.optim.Adam([voxels_src], lr = args.lr)
+    global q
+    srcs = []
+    qidx = 0
     for step in range(start_iter, args.max_iter):
         iter_start_time = time.time()
 
-        loss = losses.voxel_loss(voxels_src,voxels_tgt)
+        loss = losses.voxel_loss(torch.sigmoid(voxels_src),voxels_tgt)
 
         optimizer.zero_grad()
         loss.backward()
@@ -102,10 +127,53 @@ def fit_voxel(voxels_src, voxels_tgt, args):
         iter_time = time.time() - iter_start_time
 
         loss_vis = loss.cpu().item()
+        if loss_vis < tol:
+            print('Converged!')
+            break
+        
+        if step%q[qidx]==0:
+            srcs.append((step,torch.sigmoid(voxels_src).clone()))
+            qidx = min(qidx+1, len(q)-1)
 
         print("[%4d/%4d]; ttime: %.0f (%.2f); loss: %.3f" % (step, args.max_iter, total_time,  iter_time, loss_vis))
     
     print('Done!')
+    return srcs
+
+def plot(srcs: list, tgt, type: str):
+    
+    if type == "vox":
+        srcObj = threeDObject("Source", "vox", srcs[0][1][0,:,:,:], args.device)
+        targObj = threeDObject("Target", "vox", tgt[0,:,:,:], args.device)
+    elif type == "point":
+        srcObj = threeDObject("Source", "point", Pointclouds(points=srcs[0][1], features=torch.zeros_like(srcs[0][1])), args.device)
+        targObj = threeDObject("Target", "point", Pointclouds(points=tgt, features=torch.zeros_like(tgt)), args.device)
+    elif type == "mesh":
+        srcs[0][1].textures = pytorch3d.renderer.TexturesVertex((torch.ones_like( srcs[0][1].verts_packed()) * torch.tensor((0,0,0)).to(args.device)).unsqueeze(0))
+        tgt.textures = pytorch3d.renderer.TexturesVertex((torch.ones_like( tgt.verts_packed()) * torch.tensor((0,0,0)).to(args.device)).unsqueeze(0))
+        srcObj = threeDObject("Source", "mesh",  srcs[0][1], args.device)
+        targObj = threeDObject("Target", "mesh", tgt, args.device)
+    
+    cam_angles = np.linspace(0, 3*np.pi, len(srcs))
+    images_list = []
+    for (step,src), cam_angle in zip(srcs, cam_angles):
+        if type == "vox":
+            srcObj.update(data=src[0,:,:,:], name=f"Optimizer Iteration: {step}")
+        elif type == "mesh":
+            src.textures =  pytorch3d.renderer.TexturesVertex((torch.ones_like( src.verts_packed()) * torch.tensor((0,0,0)).to(args.device)).unsqueeze(0))
+            srcObj.update(data=src, name=f"Optimizer Iteration: {step}")
+        elif type == "point":
+            srcObj.update(data=Pointclouds(points=src, features=torch.zeros_like(src)), name=f"Optimizer Iteration: {step}")
+
+        R, T = pytorch3d.renderer.cameras.look_at_view_transform(dist=2.5, elev=0, azim=cam_angle, degrees=False)
+        cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, fov=60, device=args.device)
+
+        image = renderObjects(objects=[srcObj, targObj],cameras=cameras)
+        images_list.append(image)
+
+    full_path = args.output_path / (f'fit_{args.type}.gif')
+    imageio.mimsave(full_path, images_list, fps=30)
+    print(f'Saved the 360 GIF for fitting {args.type} in {full_path}')
 
 
 def train_model(args):
@@ -120,6 +188,7 @@ def train_model(args):
         if torch.is_tensor(feed[k]):
             feed_cuda[k] = feed[k].to(args.device).float()
 
+    global q 
 
     if args.type == "vox":
         # initialization
@@ -128,7 +197,9 @@ def train_model(args):
         voxels_tgt = feed_cuda['voxels']
 
         # fitting
-        fit_voxel(voxels_src, voxels_tgt, args)
+        q = generate_intervals(args.max_iter, mapping={0: 100})
+        srcs = fit_voxel(voxels_src, voxels_tgt, args)
+        plot(srcs, voxels_tgt, "vox") 
 
 
     elif args.type == "point":
@@ -138,7 +209,9 @@ def train_model(args):
         pointclouds_tgt = sample_points_from_meshes(mesh_tgt, args.n_points)
 
         # fitting
-        fit_pointcloud(pointclouds_src, pointclouds_tgt, args)        
+        q = generate_intervals(args.max_iter, mapping={0: 100})
+        srcs = fit_pointcloud(pointclouds_src, pointclouds_tgt, args) 
+        plot(srcs, pointclouds_tgt, "point") 
     
     elif args.type == "mesh":
         # initialization
@@ -147,9 +220,22 @@ def train_model(args):
         mesh_tgt = Meshes(verts=[feed_cuda['verts']], faces=[feed_cuda['faces']])
 
         # fitting
-        fit_mesh(mesh_src, mesh_tgt, args)        
+        q = generate_intervals(args.max_iter, mapping={0: 100})
+        srcs = fit_mesh(mesh_src, mesh_tgt, args)        
+        plot(srcs, mesh_tgt, "mesh") 
 
 
+def generate_intervals(N, mapping={0: 100, 400: 200, 2000: 500, 5000: 1000}):
+    step_size = 0 
+    log_at = [] 
+    step = 0  
+    while step < N:
+        log_at.append(step)
+        if step in mapping:
+            step_size = mapping[step]
+        step += step_size
+    log_at[0] = 1
+    return log_at
     
     
 
---
+++ render.py
@@ -0,0 +1,148 @@
+from pathlib import Path
+import pytorch3d as p3d
+import torch
+import numpy as np
+import cv2
+from pytorch3d.renderer import (
+    AlphaCompositor,
+    RasterizationSettings,
+    MeshRenderer,
+    MeshRasterizer,
+    PointsRasterizationSettings,
+    PointsRenderer,
+    PointsRasterizer,
+    HardPhongShader,)
+from pytorch3d.structures import Pointclouds, Meshes
+
+class threeDObject:
+    name: str
+    dtype: str
+    data: any
+    device: torch.device
+    renderer: p3d.renderer
+
+    def __init__(self, name: str, dtype: str, data: any, device: torch.device, 
+                 renderer: p3d.renderer=None):
+        
+        self.name = name
+        self.device = device
+        self.dtype = dtype
+        self.update(data=data)
+        
+        if renderer is None:
+            self.renderer = renderer
+            self.setRenderer()
+
+    def setRenderer(self, renderer: p3d.renderer=None, image_size=512, 
+                    lights=None, radius=0.01, background_color=(1, 1, 1)):
+                    
+        if renderer is None:
+            if self.dtype == "mesh":
+                raster_settings = RasterizationSettings(image_size=image_size, blur_radius=0.0, faces_per_pixel=1,)
+                self.renderer = MeshRenderer(rasterizer=MeshRasterizer(raster_settings=raster_settings),
+                            shader=HardPhongShader(device=self.device, lights=lights))
+                
+            elif self.dtype == "point":
+                raster_settings = PointsRasterizationSettings(image_size=image_size, radius=radius,)
+                self.renderer = PointsRenderer(rasterizer=PointsRasterizer(raster_settings=raster_settings),
+                                                compositor=AlphaCompositor(background_color=background_color),)
+            elif  self.dtype == "vox":
+                # raster_settings = PointsRasterizationSettings(image_size=image_size, radius=radius,)
+                # self.renderer = PointsRenderer(rasterizer=PointsRasterizer(raster_settings=raster_settings),
+                #                                 compositor=AlphaCompositor(background_color=background_color),)
+                raster_settings = RasterizationSettings(image_size=image_size, blur_radius=0.0, faces_per_pixel=1,)
+                self.renderer = MeshRenderer(rasterizer=MeshRasterizer(raster_settings=raster_settings),
+                            shader=HardPhongShader(device=self.device, lights=lights))
+                
+                # raise ValueError("Required to specify renderer for voxel type. Points or Mesh renderer can be used.")
+
+            else:
+                raise ValueError("Invalid dtype, renderer not defined")
+        else:
+            self.renderer = renderer
+            
+    def update(self, name=None, data=None):
+        if self.dtype=='point':
+            if not isinstance(data, Pointclouds):
+                raise ValueError("Expected data to be of type pytorch3D.Structures.Pointclouds")
+            self.data = data
+
+        elif self.dtype=='mesh':
+            if not isinstance(data, Meshes):
+                raise ValueError("Expected data to be of type pytorch3D.Structures.Meshes")
+            self.data = data
+
+        elif self.dtype=='vox':
+            if not isinstance(data, torch.Tensor):
+                raise ValueError("Invalid data type for voxel or missing coordinates")
+            self.data = data
+        
+        self.name = name if name is not None else self.name
+
+        
+    def render(self, cameras:p3d.renderer.FoVPerspectiveCameras) -> np.array:
+
+        if self.dtype == "vox":
+            if isinstance(self.renderer, PointsRenderer):
+                raise('Unable to render voxel as point cloud')
+                #Render the voxel as point cloud, Pytorch3D does not support voxel rendering
+                #Convert voxel grid to point cloud
+                #Each voxel's center is considered as a point
+                # indices = torch.nonzero(self.data)
+                # print(self.data.shape)
+                # print(indices.shape)
+                # # points = voxel_indices
+                # data = Pointclouds(points=[points])
+
+            elif isinstance(self.renderer, MeshRenderer):
+                data = p3d.ops.cubify(self.data.unsqueeze(0), thresh=0.5) 
+                data.textures = p3d.renderer.TexturesVertex((torch.ones_like(data.verts_packed()) * torch.tensor((0,0,0)).to(self.device)).unsqueeze(0))
+                
+        else:
+            data = self.data 
+
+        rendered = self.renderer(data, cameras=cameras)
+        img = rendered.cpu().detach().numpy()[0, ..., :3]  # (B, H, W, 4) -> (H, W, 3)
+        img = np.clip(img * 255, 0, 255).astype(np.uint8) #for compatability with PIL
+        return addTextToImage(img, self.name)
+
+
+def renderObjects(objects: list[threeDObject], cameras=p3d.renderer.FoVPerspectiveCameras, space_width=10):
+    img = None
+    space_color = (255, 255, 255)
+    
+    for obj in objects:
+        if img is None:
+            img = obj.render(cameras)
+        else:
+            space = np.full((img.shape[0], space_width, img.shape[2]), space_color, dtype=np.uint8)
+            img = np.concatenate((img, space, obj.render(cameras)), axis=1)
+
+    return img
+
+def addTextToImage(image, text):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    color = (0, 0, 0)  # Text color
+    thickness = 2
+
+    # Get the size of the text
+    text_size, _ = cv2.getTextSize(text, font, font_scale, thickness)
+    
+    # Calculate the amount of space needed for the text
+    text_height = text_size[1] + 30  # Adding some extra space for padding
+
+    # Create a new image with extra space for the text at the top
+    new_image_height = image.shape[0] + text_height
+    new_image = np.full((new_image_height, image.shape[1], 3), (255,255,255), dtype=np.uint8)
+    new_image[text_height:,:,:] = image  # Copy the original image starting below the new blank space
+    
+    # Calculate the position to place the text
+    # Text will be placed in the new blank space, so we adjust `text_y` accordingly
+    text_x = (new_image.shape[1] - text_size[0]) // 2
+    text_y = text_size[1] + 10  # Adjusted for the new space
+
+    # Add the text to the new image
+    cv2.putText(new_image, text, (text_x, text_y), font, font_scale, color, thickness)
+
+    return new_image
